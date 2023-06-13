7bce1568ddbdbb4c3f57370366bf66ea78a84add
naru03 <narurei03@gmail.com>
2023-06-13 13:41:04 +0900

    ファイル読み込み

--- RBS/Form1.Designer.cs
+++ RBS/Form1.Designer.cs
@@ -28,18 +28,108 @@ partial class Form1
     /// </summary>
     private void InitializeComponent()
     {
+        Label title;
+        button1 = new Button();
+        label1 = new Label();
+        linkLabel1 = new LinkLabel();
+        openFileDialog1 = new OpenFileDialog();
+        textBox1 = new TextBox();
+        dataGridView1 = new DataGridView();
+        title = new Label();
+        ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
         SuspendLayout();
+        // 
+        // title
+        // 
+        title.AutoSize = true;
+        title.Font = new Font("ＭＳ ゴシック", 26.25F, FontStyle.Regular, GraphicsUnit.Point);
+        title.Location = new Point(158, 26);
+        title.Name = "title";
+        title.Size = new Size(303, 35);
+        title.TabIndex = 1;
+        title.Text = "落単防止システム";
+        title.Click += title_Click;
+        // 
+        // button1
+        // 
+        button1.Font = new Font("ＭＳ ゴシック", 9F, FontStyle.Regular, GraphicsUnit.Point);
+        button1.Location = new Point(498, 122);
+        button1.Name = "button1";
+        button1.Size = new Size(88, 23);
+        button1.TabIndex = 0;
+        button1.Text = "選択";
+        button1.UseVisualStyleBackColor = true;
+        button1.Click += button1_Click;
+        // 
+        // label1
+        // 
+        label1.AutoSize = true;
+        label1.Font = new Font("ＭＳ ゴシック", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+        label1.Location = new Point(27, 124);
+        label1.Name = "label1";
+        label1.Size = new Size(135, 15);
+        label1.TabIndex = 2;
+        label1.Text = "ファイル読み込み";
+        // 
+        // linkLabel1
+        // 
+        linkLabel1.AutoSize = true;
+        linkLabel1.Font = new Font("ＭＳ ゴシック", 9F, FontStyle.Regular, GraphicsUnit.Point);
+        linkLabel1.Location = new Point(27, 87);
+        linkLabel1.Name = "linkLabel1";
+        linkLabel1.Size = new Size(125, 12);
+        linkLabel1.TabIndex = 3;
+        linkLabel1.TabStop = true;
+        linkLabel1.Text = "ファイル作成のやり方";
+        linkLabel1.LinkClicked += linkLabel1_LinkClicked;
+        // 
+        // openFileDialog1
+        // 
+        openFileDialog1.FileName = "openFileDialog1";
+        openFileDialog1.Filter = "HTML files (*.html;*.htm)|*.html;*.htm";
+        // 
+        // textBox1
+        // 
+        textBox1.Location = new Point(168, 121);
+        textBox1.Name = "textBox1";
+        textBox1.Size = new Size(324, 23);
+        textBox1.TabIndex = 4;
+        // 
+        // dataGridView1
+        // 
+        dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+        dataGridView1.Location = new Point(27, 183);
+        dataGridView1.Name = "dataGridView1";
+        dataGridView1.RowTemplate.Height = 25;
+        dataGridView1.Size = new Size(1167, 204);
+        dataGridView1.TabIndex = 5;
         // 
         // Form1
         // 
         AutoScaleDimensions = new SizeF(7F, 15F);
         AutoScaleMode = AutoScaleMode.Font;
-        ClientSize = new Size(800, 450);
+        ClientSize = new Size(1224, 399);
+        Controls.Add(dataGridView1);
+        Controls.Add(textBox1);
+        Controls.Add(linkLabel1);
+        Controls.Add(label1);
+        Controls.Add(title);
+        Controls.Add(button1);
         Name = "Form1";
         Text = "Form1";
         Load += Form1_Load;
+        ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
         ResumeLayout(false);
+        PerformLayout();
     }
 
     #endregion
+
+    private Button button1;
+    private Label title;
+    private Label label1;
+    private LinkLabel linkLabel1;
+    private OpenFileDialog openFileDialog1;
+    private TextBox textBox1;
+    private DataGridView dataGridView1;
 }
--- RBS/Form1.cs
+++ RBS/Form1.cs
@@ -1,3 +1,15 @@
+using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Security;
+using System.Text;
+using System.Windows.Forms;
+
 namespace RBS;
 
 public partial class Form1 : Form
@@ -8,6 +20,64 @@ public partial class Form1 : Form
     }
 
     private void Form1_Load(object sender, EventArgs e)
+    {
+        dataGridView1.ColumnCount = 11;
+        dataGridView1.Columns[0].HeaderText = "系・郡";
+        dataGridView1.Columns[1].HeaderText = "科目名";
+        dataGridView1.Columns[2].HeaderText = "科目";
+        dataGridView1.Columns[3].HeaderText = "単位区分";
+        dataGridView1.Columns[4].HeaderText = "単位数";
+        dataGridView1.Columns[5].HeaderText = "判定";
+        dataGridView1.Columns[6].HeaderText = "評価";
+        dataGridView1.Columns[7].HeaderText = "年次";
+        dataGridView1.Columns[8].HeaderText = "年度";
+        dataGridView1.Columns[9].HeaderText = "期";
+        dataGridView1.Columns[10].HeaderText = "教員";
+        dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+        DialogResult dr = openFileDialog1.ShowDialog();
+        if (dr == System.Windows.Forms.DialogResult.OK)
+        {
+            try
+            {
+                String[] Kamoku = new string[11];
+                var sr = new StreamReader(openFileDialog1.FileName);
+                textBox1.Text = openFileDialog1.FileName;
+                string htmlStr = sr.ReadToEnd();
+                if (htmlStr != null)
+                {
+                    var htmlDoc = new HtmlAgilityPack.HtmlDocument();
+                    htmlDoc.LoadHtml(htmlStr);
+                    HtmlNodeCollection nodes = htmlDoc.DocumentNode.SelectNodes("//td[@id='kyuukou' or @id='kyuukou1']");
+                    int i = 0;
+                    foreach (HtmlNode node in nodes)
+                    {
+                        Kamoku[i % 11] = node.InnerText;
+                        i++;
+                        if (i % 11 == 0)
+                        {
+                            dataGridView1.Rows.Add(Kamoku[0], Kamoku[1], Kamoku[2], Kamoku[3], Kamoku[4], Kamoku[5], Kamoku[6], Kamoku[7], Kamoku[8], Kamoku[9], Kamoku[10]);
+                        }
+                    }
+                }
+            }
+            catch (SecurityException ex)
+            {
+                MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                $"Details:\n\n{ex.StackTrace}");
+            }
+        }
+    }
+
+    private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+    {
+
+    }
+
+    private void title_Click(object sender, EventArgs e)
     {
 
     }
