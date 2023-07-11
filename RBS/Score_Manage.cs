079386ecf90ee99ffec111b59dcf75e16e8d9b5d
al21109 <al21109@shibaura-it.ac.jp>
2023-07-11 14:10:18 +0900

    もうわしは疲れた

--- RBS/Score_Manage.cs
+++ RBS/Score_Manage.cs
@@ -1,5 +1,5 @@
 ﻿/*******************************************************************
-*** File Name : Score_Manage.cs
+*** File Name : Scoremanage.cs
 *** Version : V1.1
 *** Designer : 植竹　航也
 *** Date : 2023.07.03
@@ -10,7 +10,7 @@
 *** Revision :
 *** V1.0 : 白石 京馬, 2023.06.26
 *** V1.1 : 植竹 航也, 2023.07.03
-*** V1.2 : 修正者名, yyyy.mm.dd 改訂モジュール名を書く
+*** V1.2 : 篠塚 勇希, 2023.07.11 Scoremanage
 *** V1.3 : 修正者名, yyyy.mm.dd 改訂モジュール名を書く
 */
 
@@ -27,7 +27,6 @@ using System.Globalization;
 
 namespace RBS
 {
-    
     public partial class Score_Manage
     {
 
@@ -56,17 +55,30 @@ namespace RBS
         public static int gengosum_max = 0;
         public static int taikusum_max = 0;
         public static int suurisum_max = 0;
-        public static int zengaku_max= 0;
+        public static int zengaku_max = 0;
         public static int kyoutuu_max = 0;
         public static int gakkakateigai_max = 0;
 
-        /****************************************************************************
-        *** Constructor  Name : Score_Manage()
-        *** Designer : 白石 京馬
-        *** Date : 2023.6.26
-        *** Method : ファイル読み込みで読み込んだファイルから単位の計算を行う。
-        *** Return : ×
-        ****************************************************************************/
+        public static int graph_suuri1sum = 0;
+        public static int graph_suuri2sum = 0;
+        public static int graph_suuri3sum = 0;
+        public static int graph_suuri4sum = 0;
+        public static int graph_genngo1sum = 0;
+        public static int graph_genngo2sum = 0;
+        public static int graph_genngo3sum = 0;
+        public static int graph_genngo4sum = 0;
+        public static int graph_zinnbunn1sum = 0;
+        public static int graph_zinnbunn2sum = 0;
+        public static int graph_zinnbunn3sum = 0;
+        public static int graph_zinnbunn4sum = 0;
+        public static int graph_taiiku1sum = 0;
+        public static int graph_taiiku2sum = 0;
+        public static int graph_taiiku3sum = 0;
+        public static int graph_taiiku4sum = 0;
+        public static int graph_sennmonn1sum = 0;
+        public static int graph_sennmonn2sum = 0;
+        public static int graph_sennmonn3sum = 0;
+        public static int graph_sennmonn4sum = 0;
 
         public Score_Manage()
         {
@@ -91,6 +103,7 @@ namespace RBS
             //public static int gakkakateigai_max = 0;
             for (int i = 0; i < scoresize; i++)
             {
+
                 if (File_Input.Kamoku[i, 0] == "学科課程外科目")
                 {
                     if (File_Input.Kamoku[i, 5] == "合格" || File_Input.Kamoku[i, 5] == "認定")
@@ -195,6 +208,92 @@ namespace RBS
                         kyoutuu_max = kyoutuu_max + int.Parse(File_Input.Kamoku[i, 4]);
                         allscore_max = allscore_max + int.Parse(File_Input.Kamoku[i, 4]);
                     }
+                }
+
+                if (File_Input.Kamoku[i, 0] == "数理基礎" && File_Input.Kamoku[i, 7] == "1" && File_Input.Kamoku[i, 5] == "合格")
+                {
+                    graph_suuri1sum += int.Parse(File_Input.Kamoku[i, 4]);
+
+                }
+                else if (File_Input.Kamoku[i, 0] == "数理基礎" && File_Input.Kamoku[i, 7] == "2" && File_Input.Kamoku[i, 5] == "合格")
+                {
+                    graph_suuri2sum += int.Parse(File_Input.Kamoku[i, 4]);
+                }
+                else if (File_Input.Kamoku[i, 0] == "数理基礎" && File_Input.Kamoku[i, 7] == "3" && File_Input.Kamoku[i, 5] == "合格")
+                {
+                    graph_suuri3sum += int.Parse(File_Input.Kamoku[i, 4]);
+                }
+                else if (File_Input.Kamoku[i, 0] == "数理基礎" && File_Input.Kamoku[i, 7] == "4" && File_Input.Kamoku[i, 5] == "合格")
+                {
+                    graph_suuri4sum += int.Parse(File_Input.Kamoku[i, 4]);
+                }
+                if (File_Input.Kamoku[i, 0] == "言語" && File_Input.Kamoku[i, 7] == "1" && File_Input.Kamoku[i, 5] == "合格")
+                {
+                    graph_genngo1sum += int.Parse(File_Input.Kamoku[i, 4]);
+
+                }
+                else if (File_Input.Kamoku[i, 0] == "言語" && File_Input.Kamoku[i, 7] == "2" && File_Input.Kamoku[i, 5] == "合格")
+                {
+                    graph_genngo2sum += int.Parse(File_Input.Kamoku[i, 4]);
+                }
+                else if (File_Input.Kamoku[i, 0] == "言語" && File_Input.Kamoku[i, 7] == "3" && File_Input.Kamoku[i, 5] == "合格")
+                {
+                    graph_genngo3sum += int.Parse(File_Input.Kamoku[i, 4]);
+                }
+                else if (File_Input.Kamoku[i, 0] == "言語" && File_Input.Kamoku[i, 7] == "4" && File_Input.Kamoku[i, 5] == "合格")
+                {
+                    graph_genngo4sum += int.Parse(File_Input.Kamoku[i, 4]);
+                }
+                if (File_Input.Kamoku[i, 0] == "人文社会系教養" && File_Input.Kamoku[i, 7] == "1" && File_Input.Kamoku[i, 5] == "合格")
+                {
+                    graph_zinnbunn1sum += int.Parse(File_Input.Kamoku[i, 4]);
+
+                }
+                else if (File_Input.Kamoku[i, 0] == "人文社会系教養" && File_Input.Kamoku[i, 7] == "2" && File_Input.Kamoku[i, 5] == "合格")
+                {
+                    graph_zinnbunn2sum += int.Parse(File_Input.Kamoku[i, 4]);
+                }
+                else if (File_Input.Kamoku[i, 0] == "人文社会系教養" && File_Input.Kamoku[i, 7] == "3" && File_Input.Kamoku[i, 5] == "合格")
+                {
+                    graph_zinnbunn3sum += int.Parse(File_Input.Kamoku[i, 4]);
+                }
+                else if (File_Input.Kamoku[i, 0] == "人文社会系教養" && File_Input.Kamoku[i, 7] == "4" && File_Input.Kamoku[i, 5] == "合格")
+                {
+                    graph_zinnbunn4sum += int.Parse(File_Input.Kamoku[i, 4]);
+                }
+                if (File_Input.Kamoku[i, 0] == "体育健康" && File_Input.Kamoku[i, 7] == "1" && File_Input.Kamoku[i, 5] == "合格")
+                {
+                    graph_taiiku1sum += int.Parse(File_Input.Kamoku[i, 4]);
+
+                }
+                else if (File_Input.Kamoku[i, 0] == "体育健康" && File_Input.Kamoku[i, 7] == "2" && File_Input.Kamoku[i, 5] == "合格")
+                {
+                    graph_taiiku2sum += int.Parse(File_Input.Kamoku[i, 4]);
+                }
+                else if (File_Input.Kamoku[i, 0] == "体育健康" && File_Input.Kamoku[i, 7] == "3" && File_Input.Kamoku[i, 5] == "合格")
+                {
+                    graph_taiiku3sum += int.Parse(File_Input.Kamoku[i, 4]);
+                }
+                else if (File_Input.Kamoku[i, 0] == "体育健康" && File_Input.Kamoku[i, 7] == "4" && File_Input.Kamoku[i, 5] == "合格")
+                {
+                    graph_taiiku4sum += int.Parse(File_Input.Kamoku[i, 4]);
+                }
+                if (File_Input.Kamoku[i, 0] == "専門" && File_Input.Kamoku[i, 7] == "1" && File_Input.Kamoku[i, 5] == "合格")
+                {
+                    graph_sennmonn1sum += int.Parse(File_Input.Kamoku[i, 4]);
+
+                }
+                else if (File_Input.Kamoku[i, 0] == "専門" && File_Input.Kamoku[i, 7] == "2" && File_Input.Kamoku[i, 5] == "合格")
+                {
+                    graph_sennmonn2sum += int.Parse(File_Input.Kamoku[i, 4]);
+                }
+                else if (File_Input.Kamoku[i, 0] == "専門" && File_Input.Kamoku[i, 7] == "3" && File_Input.Kamoku[i, 5] == "合格")
+                {
+                    graph_sennmonn3sum += int.Parse(File_Input.Kamoku[i, 4]);
+                }
+                else if (File_Input.Kamoku[i, 0] == "専門" && File_Input.Kamoku[i, 7] == "4" && File_Input.Kamoku[i, 5] == "合格")
+                {
+                    graph_sennmonn4sum += int.Parse(File_Input.Kamoku[i, 4]);
                 }
             }
 
