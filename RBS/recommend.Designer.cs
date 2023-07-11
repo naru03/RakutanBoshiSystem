414f1c8b42215022c398a5dc70fd3ff164d05682
naru03 <narurei03@gmail.com>
2023-07-11 14:26:11 +0900

    レイアウト調整

--- RBS/recommend.Designer.cs
+++ RBS/recommend.Designer.cs
@@ -79,12 +79,13 @@ namespace RBS
             dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3, Column4, Column5, Column6, Column7, Column11, Column12, Column13 });
-            dataGridView1.Location = new Point(350, 101);
-            dataGridView1.Margin = new Padding(2);
+            dataGridView1.Location = new Point(245, 61);
+            dataGridView1.Margin = new Padding(1);
             dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
             dataGridView1.RowHeadersWidth = 62;
             dataGridView1.RowTemplate.Height = 27;
-            dataGridView1.Size = new Size(1132, 92);
+            dataGridView1.Size = new Size(792, 55);
             dataGridView1.TabIndex = 0;
             // 
             // Column1
@@ -92,83 +93,101 @@ namespace RBS
             Column1.HeaderText = "科目区分";
             Column1.MinimumWidth = 8;
             Column1.Name = "Column1";
+            Column1.ReadOnly = true;
             // 
             // Column2
             // 
             Column2.HeaderText = "数理";
             Column2.MinimumWidth = 8;
             Column2.Name = "Column2";
+            Column2.ReadOnly = true;
             // 
             // Column3
             // 
             Column3.HeaderText = "言語";
             Column3.MinimumWidth = 8;
             Column3.Name = "Column3";
+            Column3.ReadOnly = true;
             // 
             // Column4
             // 
             Column4.HeaderText = "情報";
             Column4.MinimumWidth = 8;
             Column4.Name = "Column4";
+            Column4.ReadOnly = true;
             // 
             // Column5
             // 
             Column5.HeaderText = "人文";
             Column5.MinimumWidth = 8;
             Column5.Name = "Column5";
+            Column5.ReadOnly = true;
             // 
             // Column6
             // 
             Column6.HeaderText = "体育";
             Column6.MinimumWidth = 8;
             Column6.Name = "Column6";
+            Column6.ReadOnly = true;
             // 
             // Column7
             // 
             Column7.HeaderText = "専門";
             Column7.MinimumWidth = 8;
             Column7.Name = "Column7";
+            Column7.ReadOnly = true;
             // 
             // Column11
             // 
             Column11.HeaderText = "学科外認定";
             Column11.MinimumWidth = 8;
             Column11.Name = "Column11";
+            Column11.ReadOnly = true;
             // 
             // Column12
             // 
             Column12.HeaderText = "全学共通";
             Column12.MinimumWidth = 8;
             Column12.Name = "Column12";
+            Column12.ReadOnly = true;
             // 
             // Column13
             // 
             Column13.HeaderText = "工学部共通";
             Column13.MinimumWidth = 8;
             Column13.Name = "Column13";
+            Column13.ReadOnly = true;
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new System.Drawing.Font("MS UI Gothic", 18F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(795, 45);
-            label1.Margin = new Padding(2, 0, 2, 0);
+            label1.Location = new Point(556, 27);
+            label1.Margin = new Padding(1, 0, 1, 0);
             label1.Name = "label1";
-            label1.Size = new Size(180, 36);
+            label1.Size = new Size(122, 24);
             label1.TabIndex = 1;
             label1.Text = "Raku単くん";
             // 
             // dataGridView2
             // 
+            dataGridView2.AllowUserToAddRows = false;
+            dataGridView2.AllowUserToDeleteRows = false;
+            dataGridView2.AllowUserToResizeColumns = false;
+            dataGridView2.AllowUserToResizeRows = false;
             dataGridView2.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridView2.Columns.AddRange(new DataGridViewColumn[] { Column8, Column9, Column10 });
-            dataGridView2.Location = new Point(615, 418);
-            dataGridView2.Margin = new Padding(2);
+            dataGridView2.Location = new Point(430, 251);
+            dataGridView2.Margin = new Padding(1);
+            dataGridView2.MultiSelect = false;
             dataGridView2.Name = "dataGridView2";
+            dataGridView2.ReadOnly = true;
             dataGridView2.RowHeadersWidth = 62;
             dataGridView2.RowTemplate.Height = 27;
-            dataGridView2.Size = new Size(529, 185);
+            dataGridView2.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView2.ShowCellToolTips = false;
+            dataGridView2.Size = new Size(370, 111);
             dataGridView2.TabIndex = 2;
             // 
             // Column8
@@ -176,26 +195,29 @@ namespace RBS
             Column8.HeaderText = "科目区分";
             Column8.MinimumWidth = 8;
             Column8.Name = "Column8";
+            Column8.ReadOnly = true;
             // 
             // Column9
             // 
             Column9.HeaderText = "授業名";
             Column9.MinimumWidth = 8;
             Column9.Name = "Column9";
+            Column9.ReadOnly = true;
             // 
             // Column10
             // 
             Column10.HeaderText = "単位数";
             Column10.MinimumWidth = 8;
             Column10.Name = "Column10";
+            Column10.ReadOnly = true;
             // 
             // button1
             // 
             button1.Font = new System.Drawing.Font("MS UI Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            button1.Location = new Point(1244, 530);
-            button1.Margin = new Padding(2);
+            button1.Location = new Point(871, 318);
+            button1.Margin = new Padding(1);
             button1.Name = "button1";
-            button1.Size = new Size(134, 78);
+            button1.Size = new Size(94, 47);
             button1.TabIndex = 3;
             button1.Text = "おしまい";
             button1.UseVisualStyleBackColor = true;
@@ -205,10 +227,10 @@ namespace RBS
             // 
             label2.AutoSize = true;
             label2.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            label2.Location = new Point(755, 208);
-            label2.Margin = new Padding(2, 0, 2, 0);
+            label2.Location = new Point(528, 125);
+            label2.Margin = new Padding(1, 0, 1, 0);
             label2.Name = "label2";
-            label2.Size = new Size(220, 32);
+            label2.Size = new Size(147, 21);
             label2.TabIndex = 4;
             label2.Text = "総単位数(取得済み)";
             // 
@@ -216,10 +238,10 @@ namespace RBS
             // 
             label3.AutoSize = true;
             label3.Font = new System.Drawing.Font("Yu Gothic UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
-            label3.Location = new Point(278, 465);
-            label3.Margin = new Padding(2, 0, 2, 0);
+            label3.Location = new Point(195, 279);
+            label3.Margin = new Padding(1, 0, 1, 0);
             label3.Name = "label3";
-            label3.Size = new Size(279, 38);
+            label3.Size = new Size(189, 25);
             label3.TabIndex = 5;
             label3.Text = "あなたにおすすめの単位";
             // 
@@ -227,10 +249,10 @@ namespace RBS
             // 
             label4.AutoSize = true;
             label4.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            label4.Location = new Point(1006, 208);
-            label4.Margin = new Padding(2, 0, 2, 0);
+            label4.Location = new Point(704, 125);
+            label4.Margin = new Padding(1, 0, 1, 0);
             label4.Name = "label4";
-            label4.Size = new Size(80, 32);
+            label4.Size = new Size(54, 21);
             label4.TabIndex = 6;
             label4.Text = "label4";
             // 
@@ -238,10 +260,10 @@ namespace RBS
             // 
             label6.AutoSize = true;
             label6.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            label6.Location = new Point(755, 380);
-            label6.Margin = new Padding(2, 0, 2, 0);
+            label6.Location = new Point(528, 228);
+            label6.Margin = new Padding(1, 0, 1, 0);
             label6.Name = "label6";
-            label6.Size = new Size(222, 32);
+            label6.Size = new Size(148, 21);
             label6.TabIndex = 8;
             label6.Text = "総単位数(取得予定)";
             // 
@@ -250,12 +272,13 @@ namespace RBS
             dataGridView3.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             dataGridView3.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridView3.Columns.AddRange(new DataGridViewColumn[] { dataGridViewTextBoxColumn1, dataGridViewTextBoxColumn2, dataGridViewTextBoxColumn3, dataGridViewTextBoxColumn4, dataGridViewTextBoxColumn5, dataGridViewTextBoxColumn6, dataGridViewTextBoxColumn7, dataGridViewTextBoxColumn8, dataGridViewTextBoxColumn9, dataGridViewTextBoxColumn10 });
-            dataGridView3.Location = new Point(350, 272);
-            dataGridView3.Margin = new Padding(2);
+            dataGridView3.Location = new Point(245, 163);
+            dataGridView3.Margin = new Padding(1);
             dataGridView3.Name = "dataGridView3";
+            dataGridView3.ReadOnly = true;
             dataGridView3.RowHeadersWidth = 62;
             dataGridView3.RowTemplate.Height = 27;
-            dataGridView3.Size = new Size(1132, 92);
+            dataGridView3.Size = new Size(792, 55);
             dataGridView3.TabIndex = 9;
             // 
             // dataGridViewTextBoxColumn1
@@ -263,79 +286,89 @@ namespace RBS
             dataGridViewTextBoxColumn1.HeaderText = "科目区分";
             dataGridViewTextBoxColumn1.MinimumWidth = 8;
             dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            dataGridViewTextBoxColumn1.ReadOnly = true;
             // 
             // dataGridViewTextBoxColumn2
             // 
             dataGridViewTextBoxColumn2.HeaderText = "数理";
             dataGridViewTextBoxColumn2.MinimumWidth = 8;
             dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            dataGridViewTextBoxColumn2.ReadOnly = true;
             // 
             // dataGridViewTextBoxColumn3
             // 
             dataGridViewTextBoxColumn3.HeaderText = "言語";
             dataGridViewTextBoxColumn3.MinimumWidth = 8;
             dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            dataGridViewTextBoxColumn3.ReadOnly = true;
             // 
             // dataGridViewTextBoxColumn4
             // 
             dataGridViewTextBoxColumn4.HeaderText = "情報";
             dataGridViewTextBoxColumn4.MinimumWidth = 8;
             dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            dataGridViewTextBoxColumn4.ReadOnly = true;
             // 
             // dataGridViewTextBoxColumn5
             // 
             dataGridViewTextBoxColumn5.HeaderText = "人文";
             dataGridViewTextBoxColumn5.MinimumWidth = 8;
             dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
+            dataGridViewTextBoxColumn5.ReadOnly = true;
             // 
             // dataGridViewTextBoxColumn6
             // 
             dataGridViewTextBoxColumn6.HeaderText = "体育";
             dataGridViewTextBoxColumn6.MinimumWidth = 8;
             dataGridViewTextBoxColumn6.Name = "dataGridViewTextBoxColumn6";
+            dataGridViewTextBoxColumn6.ReadOnly = true;
             // 
             // dataGridViewTextBoxColumn7
             // 
             dataGridViewTextBoxColumn7.HeaderText = "専門";
             dataGridViewTextBoxColumn7.MinimumWidth = 8;
             dataGridViewTextBoxColumn7.Name = "dataGridViewTextBoxColumn7";
+            dataGridViewTextBoxColumn7.ReadOnly = true;
             // 
             // dataGridViewTextBoxColumn8
             // 
             dataGridViewTextBoxColumn8.HeaderText = "学科外認定";
             dataGridViewTextBoxColumn8.MinimumWidth = 8;
             dataGridViewTextBoxColumn8.Name = "dataGridViewTextBoxColumn8";
+            dataGridViewTextBoxColumn8.ReadOnly = true;
             // 
             // dataGridViewTextBoxColumn9
             // 
             dataGridViewTextBoxColumn9.HeaderText = "全学共通";
             dataGridViewTextBoxColumn9.MinimumWidth = 8;
             dataGridViewTextBoxColumn9.Name = "dataGridViewTextBoxColumn9";
+            dataGridViewTextBoxColumn9.ReadOnly = true;
             // 
             // dataGridViewTextBoxColumn10
             // 
             dataGridViewTextBoxColumn10.HeaderText = "工学部共通";
             dataGridViewTextBoxColumn10.MinimumWidth = 8;
             dataGridViewTextBoxColumn10.Name = "dataGridViewTextBoxColumn10";
+            dataGridViewTextBoxColumn10.ReadOnly = true;
             // 
             // label7
             // 
             label7.AutoSize = true;
             label7.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            label7.Location = new Point(1008, 380);
-            label7.Margin = new Padding(2, 0, 2, 0);
+            label7.Location = new Point(706, 228);
+            label7.Margin = new Padding(1, 0, 1, 0);
             label7.Name = "label7";
-            label7.Size = new Size(79, 32);
+            label7.Size = new Size(54, 21);
             label7.TabIndex = 10;
             label7.Text = "label7";
             // 
             // button2
             // 
             button2.Font = new System.Drawing.Font("Yu Gothic UI", 11F, FontStyle.Bold, GraphicsUnit.Point);
-            button2.Location = new Point(352, 564);
-            button2.Margin = new Padding(2);
+            button2.Location = new Point(246, 338);
+            button2.Margin = new Padding(1);
             button2.Name = "button2";
-            button2.Size = new Size(222, 44);
+            button2.Size = new Size(155, 26);
             button2.TabIndex = 11;
             button2.Text = "レコメンドしなおし";
             button2.UseVisualStyleBackColor = true;
@@ -343,9 +376,10 @@ namespace RBS
             // 
             // button4
             // 
-            button4.Location = new Point(1355, 445);
+            button4.Location = new Point(948, 267);
+            button4.Margin = new Padding(2);
             button4.Name = "button4";
-            button4.Size = new Size(112, 34);
+            button4.Size = new Size(78, 20);
             button4.TabIndex = 12;
             button4.Text = "学年別取得単位";
             button4.UseVisualStyleBackColor = true;
@@ -353,10 +387,10 @@ namespace RBS
             // 
             // Recommend
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.Control;
-            ClientSize = new Size(1749, 668);
+            ClientSize = new Size(1224, 401);
             Controls.Add(button4);
             Controls.Add(button2);
             Controls.Add(label7);
@@ -369,10 +403,7 @@ namespace RBS
             Controls.Add(dataGridView2);
             Controls.Add(label1);
             Controls.Add(dataGridView1);
-<<<<<<< HEAD
-            Margin = new Padding(2, 2, 2, 2);
-=======
-            Margin = new Padding(2);
+            Margin = new Padding(1);
             Name = "Recommend";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Form2";
